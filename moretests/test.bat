1ff44525027af6a637f446dcbef03d363fe44202
Jobbo <rjobling@gmail.com>
2025-02-09 13:53:41 -0500

    tests

---
+++ moretests/test.bat
@@ -0,0 +1,14 @@
+..\src\x64\Debug\abc2.exe image1.png -preview image1_flat.png -rgb -floyd -bias flat
+..\src\x64\Debug\abc2.exe image1.png -preview image1_rgb.png -rgb -floyd -bias rgb
+
+..\src\x64\Debug\abc2.exe image2.png -preview image2_flat.png -rgb -floyd -bias flat
+..\src\x64\Debug\abc2.exe image2.png -preview image2_rgb.png -rgb -floyd -bias rgb
+
+..\src\x64\Debug\abc2.exe image3.png -preview image3_flat.png -rgb -floyd -bias flat
+..\src\x64\Debug\abc2.exe image3.png -preview image3_rgb.png -rgb -floyd -bias rgb
+
+..\src\x64\Debug\abc2.exe image4.png -preview image4_flat.png -rgb -floyd -bias flat
+..\src\x64\Debug\abc2.exe image4.png -preview image4_rgb.png -rgb -floyd -bias rgb
+
+..\src\x64\Debug\abc2.exe image5.png -preview image5_flat.png -rgb -floyd -bias flat
+..\src\x64\Debug\abc2.exe image5.png -preview image5_rgb.png -rgb -floyd -bias rgb
